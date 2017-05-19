18890f45d5f1c4cc9a7a02e2f9833d6fd2930fcd
Francisco Bernal Yescas <francisco@sproutling.com>
2017-05-18 20:24:05 -0700

    Table

--- web/scripts/lookup.js
+++ web/scripts/lookup.js
@@ -3,15 +3,13 @@ function lookup() {
     var str = document.getElementById("film_number").value;
 
     if (str == "") {
-        document.getElementById("output").innerHTML = "Enter film number";
+        document.getElementById("response").innerHTML = "Film number missing";
         return;
     } else { 
         var xmlhttp = new XMLHttpRequest();
         xmlhttp.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 500) {
-                document.getElementById("readyState").innerHTML = xmlhttp.readyState;
-                document.getElementById("status").innerHTML = xmlhttp.status;
-                document.getElementById("output").innerHTML = xmlhttp.responseText;
+                document.getElementById("response").innerHTML = xmlhttp.responseText;
             }
         };
         xmlhttp.open("GET","scripts/lookup.php?film_number="+str);
--- web/scripts/lookup.php
+++ web/scripts/lookup.php
@@ -3,44 +3,33 @@ include 'conn.php';
 
 $film = intval($_GET['film_number']);
 
-//set_time_limit(0);
-
-echo "<table>
-<tr>
-    <th>Film Number</th>
-    <th>Location</th>
-    <th>Created By</th>
-    <th>Created At</th>
-    <th>Updated At</th>
-</tr>";
-
 $stmt = $conn->prepare("SELECT f.number AS film_number, u.firstName || ' ' || u.lastName AS created_by, l.name AS location, f.created_at AS created_at, f.updated_at AS updated_at FROM public.film f INNER JOIN public.user u on f.created_by = u.id INNER JOIN public.location l on f.located_at = l.id WHERE f.number = :film");
 $stmt->bindValue(':film', $film, PDO::PARAM_INT);
 $stmt->execute();
 $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+echo "<br/><br>";
+
 if (count($rows) > 0) {
+    echo "<p>Film Found</p>
+<br>
+<table>
+<tr>
+    <th>Film Number</th>
+    <th>Location</th>
+</tr>";
     foreach ($rows as $row)
     {
         echo "<tr>";
         echo "<td>" . $row['film_number'] . "</td>";
         echo "<td>" . $row['location'] . "</td>";
-        echo "<td>" . $row['created_by'] . "</td>";
-        echo "<td>" . $row['created_at'] . "</td>";
-        echo "<td>" . $row['updated_at'] . "</td>";
         echo "</tr>";
     }
+    
+    echo "</table>";
 } else {
-    echo "<tr>";
-    echo "<td>" . $film . "</td>";
-    echo "<td>Not found</td>";
-    echo "<td>--</td>";
-    echo "<td>--</td>";
-    echo "<td>--</td>";
-    echo "</tr>";
+    echo "<p>Film Not Found</p>";
 }
-
-echo "</table>";
 
 $conn->close();
 
