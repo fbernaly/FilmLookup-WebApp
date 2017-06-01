fe48a88f8d971d16cd84c166a3b57e31868b6ef7
Francisco Bernal Yescas <francisco@sproutling.com>
2017-06-01 14:13:21 -0700

    Fixed: '500' error coming from server

--- web/scripts/lookup.js
+++ web/scripts/lookup.js
@@ -1,18 +1,21 @@
-
 function lookup() {
     var str = document.getElementById("film_number").value;
 
     if (str == "") {
         document.getElementById("response").innerHTML = "Film number missing";
         return;
-    } else { 
+    } else {
         var xmlhttp = new XMLHttpRequest();
-        xmlhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 500) {
-                document.getElementById("response").innerHTML = xmlhttp.responseText;
+        xmlhttp.onreadystatechange = function () {
+            if (xmlhttp.readyState == XMLHttpRequest.DONE) {
+                if (xmlhttp.status == 200) {
+                    document.getElementById("response").innerHTML = xmlhttp.responseText;
+                } else {
+                    alert('There was a problem with the request.');
+                }
             }
         };
-        xmlhttp.open("GET","scripts/lookup.php?film_number="+str);
+        xmlhttp.open('GET', 'scripts/lookup.php?film_number=' + str);
         xmlhttp.send();
     }
 }
--- web/scripts/lookup.php
+++ web/scripts/lookup.php
@@ -1,7 +1,7 @@
 <?php
 include 'conn.php';
 
-$film = intval($_GET['film_number']);
+$film = $_GET['film_number'];
 
 $stmt = $conn->prepare("SELECT f.number AS film_number, u.firstName || ' ' || u.lastName AS created_by, l.name AS location, f.created_at AS created_at, f.updated_at AS updated_at FROM public.film f INNER JOIN public.user u on f.created_by = u.id INNER JOIN public.location l on f.located_at = l.id WHERE f.number = :film");
 $stmt->bindValue(':film', $film, PDO::PARAM_INT);
@@ -28,9 +28,9 @@ if (count($rows) > 0) {
     
     echo "</table>";
 } else {
-    echo "<p>Film Not Found</p>";
+    echo "<p>Film '$film' Not Found</p>";
 }
 
-$conn->close();
+$conn=null;
 
 ?>
