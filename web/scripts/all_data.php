09e905e6682f1b3cd59d860e1675a0057860ad6d
Francisco Bernal Yescas <francisco@sproutling.com>
2017-06-06 01:36:53 -0700

    order by

--- web/scripts/all_data.php
+++ web/scripts/all_data.php
@@ -11,7 +11,7 @@ echo "<table>
     <th>Delete</th>
 </tr>";
 
-foreach ($conn->query("SELECT f.number AS film_number, u.firstName || ' ' || u.lastName AS created_by, l.name AS location, f.created_at AS created_at, f.updated_at AS updated_at FROM public.film f INNER JOIN public.user u on f.created_by = u.id INNER JOIN public.location l on f.located_at = l.id") as $row)
+foreach ($conn->query("SELECT f.number AS film_number, u.firstName || ' ' || u.lastName AS created_by, l.name AS location, f.created_at AS created_at, f.updated_at AS updated_at FROM public.film f INNER JOIN public.user u on f.created_by = u.id INNER JOIN public.location l on f.located_at = l.id ORDER BY f.number") as $row)
 {
     echo "<tr>";
     echo "<td>" . $row['film_number'] . "</td>";
